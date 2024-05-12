5249dfc72ee2559c8d6982e0ffdcd066b1c4a89b
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-12 21:47:04 +0300

    work

--- raspap_ins.sh
+++ raspap_ins.sh
@@ -1,4 +1,4 @@
 # sudo apt-get update
 # sudo apt-get full-upgrade
-# sudo apt-get install dhcpcd5
-sudo apt-get install lighttpd
+# sudo apt-get install dhcpcd5 lighttpd
+sudo apt-get install hostapd

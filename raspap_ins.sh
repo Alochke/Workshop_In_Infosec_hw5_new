f8c8451f3e8d5eabcbf6f8d55895dd92d85664e9
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-12 22:12:58 +0300

    work

--- raspap_ins.sh
+++ raspap_ins.sh
@@ -1,4 +1,4 @@
 # sudo apt-get update
 # sudo apt-get full-upgrade
-# sudo apt-get install dhcpcd5 lighttpd hostapd dnsmasq iptables-persistent vnstat
-sudo apt-get install qrencode
+# sudo apt-get install dhcpcd5 lighttpd hostapd dnsmasq iptables-persistent vnstat qrencode
+sudo apt-get install php7.0-cgi

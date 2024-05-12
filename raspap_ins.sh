c47c7cecd950a3162634dbd3ba78c28007ce9dac
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-12 21:57:06 +0300

    work

--- raspap_ins.sh
+++ raspap_ins.sh
@@ -1,4 +1,4 @@
 # sudo apt-get update
 # sudo apt-get full-upgrade
-# sudo apt-get install dhcpcd5 lighttpd hostapd dnsmasq
-sudo apt-get install iptables-persistent
+# sudo apt-get install dhcpcd5 lighttpd hostapd dnsmasq iptables-persistent
+sudo apt-get install vnstat

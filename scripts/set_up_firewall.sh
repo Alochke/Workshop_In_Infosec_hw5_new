d99a8a5e207cf0c7baf2b1894f3900e956ec3053
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-13 08:30:33 +0300

    work

--- scripts/set_up_firewall.sh
+++ scripts/set_up_firewall.sh
@@ -1,6 +1,6 @@
 cd ~
 
-apt update && \
+sudo apt update && \
     sudo apt upgrade -y &&\
     sudo apt install -y wget build-essential zlib1g-dev libncurses-dev libgdbm-dev libssl-dev libsqlite3-dev libffi-dev libbz2-dev git && \
     wget https://www.python.org/ftp/python/3.7.17/Python-3.7.17.tgz  && \

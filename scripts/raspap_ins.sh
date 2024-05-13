2d536ab3f3df18e5c60b7c7b56a42a2330174bd7
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-13 08:55:36 +0300

    work.

--- scripts/raspap_ins.sh
+++ scripts/raspap_ins.sh
@@ -1,4 +1,4 @@
 # sudo apt-get update
 # sudo apt-get full-upgrade
-# sudo apt-get install dhcpcd5 lighttpd hostapd dnsmasq iptables-persistent vnstat qrencode php7.0-cgi
-sudo apt-get install jq
+# sudo apt-get install dhcpcd5 lighttpd hostapd dnsmasq iptables-persistent vnstat qrencode php7.0-cgi jq 
+sudo apt-get install isoquery

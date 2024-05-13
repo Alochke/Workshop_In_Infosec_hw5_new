5ecead2716206a447cef355e2c7988de0a342d9d
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-13 18:00:44 +0300

    work

--- scripts/raspap_init_mod_new.sh
+++ scripts/raspap_init_mod_new.sh
@@ -1,29 +1,29 @@
-sudo apt-get update
-sudo apt-get full-upgrade
-sudo apt-get install -y dhcpcd5 lighttpd hostapd dnsmasq iptables-persistent vnstat qrencode php7.0-cgi jq isoquery 
+# sudo apt-get update
+# sudo apt-get full-upgrade
+# sudo apt-get install -y dhcpcd5 lighttpd hostapd dnsmasq iptables-persistent vnstat qrencode php7.0-cgi jq isoquery 
 
-sudo lighttpd-enable-mod fastcgi-php
-sudo service lighttpd force-reload
-sudo systemctl restart lighttpd.service
-
-sudo rm -rf /var/www/html
-sudo git clone https://github.com/RaspAP/raspap-webgui /var/www/html
-
-sudo sed -i 's/$logfile = escapeshellcmd($_POST['logfile']);/$logfile = $_POST['logfile']/' /var/www/html/ajax/logging/clearlog.php
-sudo sed -i 's/$ovpncfg_id = escapeshellcmd($_POST['cfg_id']);/$ovpncfg_id = $_POST['cfg_id'];/' /var/www/html/ajax/openvpn/activate_ovpncfg.php
-sudo sed -i 's/$ovpncfg_id = escapeshellcmd($_POST['cfg_id']);/$ovpncfg_id = $_POST['cfg_id'];/' /var/www/html/ajax/openvpn/del_ovpncfg.php
-
-WEBROOT="/var/www/html"
-CONFSRC="$WEBROOT/config/50-raspap-router.conf"
-LTROOT=$(grep "server.document-root" /etc/lighttpd/lighttpd.conf | awk -F '=' '{print $2}' | tr -d " \"")
-
-HTROOT=${WEBROOT/$LTROOT}
-HTROOT=$(echo "$HTROOT" | sed -e 's/\/$//')
-awk "{gsub(\"/REPLACE_ME\",\"$HTROOT\")}1" $CONFSRC > /tmp/50-raspap-router.conf
-sudo cp /tmp/50-raspap-router.conf /etc/lighttpd/conf-available/
-
-# sudo ln -s /etc/lighttpd/conf-available/50-raspap-router.conf /etc/lighttpd/conf-enabled/50-raspap-router.conf
+# sudo lighttpd-enable-mod fastcgi-php
+# sudo service lighttpd force-reload
 # sudo systemctl restart lighttpd.service
+
+# sudo rm -rf /var/www/html
+# sudo git clone https://github.com/RaspAP/raspap-webgui /var/www/html
+
+# sudo sed -i 's/$logfile = escapeshellcmd($_POST['logfile']);/$logfile = $_POST['logfile']/' /var/www/html/ajax/logging/clearlog.php
+# sudo sed -i 's/$ovpncfg_id = escapeshellcmd($_POST['cfg_id']);/$ovpncfg_id = $_POST['cfg_id'];/' /var/www/html/ajax/openvpn/activate_ovpncfg.php
+# sudo sed -i 's/$ovpncfg_id = escapeshellcmd($_POST['cfg_id']);/$ovpncfg_id = $_POST['cfg_id'];/' /var/www/html/ajax/openvpn/del_ovpncfg.php
+
+# WEBROOT="/var/www/html"
+# CONFSRC="$WEBROOT/config/50-raspap-router.conf"
+# LTROOT=$(grep "server.document-root" /etc/lighttpd/lighttpd.conf | awk -F '=' '{print $2}' | tr -d " \"")
+
+# HTROOT=${WEBROOT/$LTROOT}
+# HTROOT=$(echo "$HTROOT" | sed -e 's/\/$//')
+# awk "{gsub(\"/REPLACE_ME\",\"$HTROOT\")}1" $CONFSRC > /tmp/50-raspap-router.conf
+# sudo cp /tmp/50-raspap-router.conf /etc/lighttpd/conf-available/
+
+sudo ln -s /etc/lighttpd/conf-available/50-raspap-router.conf /etc/lighttpd/conf-enabled/50-raspap-router.conf
+sudo systemctl restart lighttpd.service
 
 # cd /var/www/html
 # sudo cp installers/raspap.sudoers /etc/sudoers.d/090_raspap

7ec7ad79bd314518576edaa06fc66829a70920a8
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-13 18:03:19 +0300

    work

--- scripts/raspap_init_mod_new.sh
+++ scripts/raspap_init_mod_new.sh
@@ -38,13 +38,13 @@ cd /var/www/html
 # sudo chown -R www-data:www-data /var/www/html
 # sudo chown -R www-data:www-data /etc/raspap
 
-sudo mv installers/enablelog.sh /etc/raspap/hostapd
-sudo mv installers/disablelog.sh /etc/raspap/hostapd
-sudo mv installers/servicestart.sh /etc/raspap/hostapd
-sudo mv installers/debuglog.sh /etc/raspap/system
+# sudo mv installers/enablelog.sh /etc/raspap/hostapd
+# sudo mv installers/disablelog.sh /etc/raspap/hostapd
+# sudo mv installers/servicestart.sh /etc/raspap/hostapd
+# sudo mv installers/debuglog.sh /etc/raspap/system
 
-# sudo chown -c root:root /etc/raspap/hostapd/*.sh
-# sudo chmod 750 /etc/raspap/hostapd/*.sh
+sudo chown -c root:root /etc/raspap/hostapd/*.sh
+sudo chmod 750 /etc/raspap/hostapd/*.sh
 
 # sudo chown -c root:root /etc/raspap/system/*.sh
 # sudo chmod 750 /etc/raspap/system/*.sh

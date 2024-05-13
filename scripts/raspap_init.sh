10ba5f8e3788a3d82ddcd6075685b78bdeb43397
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-14 01:28:10 +0300

    work

--- scripts/raspap_init.sh
+++ scripts/raspap_init.sh
@@ -1,17 +1,17 @@
 # sudo apt-get update
 # sudo apt-get full-upgrade
 
-sudo raspi-config
-sudo rfkill unblock wlan
+# sudo raspi-config
+# sudo rfkill unblock wlan
 
 # sudo apt-get install -y lighttpd git hostapd dnsmasq iptables-persistent vnstat qrencode php7.4-cgi jq isoquery
 
-sudo lighttpd-enable-mod fastcgi-php
-sudo service lighttpd force-reload
-sudo systemctl restart lighttpd.service
+# sudo lighttpd-enable-mod fastcgi-php
+# sudo service lighttpd force-reload
+# sudo systemctl restart lighttpd.service
 
-# sudo rm -rf /var/www/html
-# sudo git clone -b 2.8.7 --depth=1 https://github.com/RaspAP/raspap-webgui /var/www/html
+sudo rm -rf /var/www/html
+sudo git clone -b 2.8.7 --depth=1 https://github.com/RaspAP/raspap-webgui /var/www/html
 
 # WEBROOT="/var/www/html"
 # CONFSRC="$WEBROOT/config/50-raspap-router.conf"

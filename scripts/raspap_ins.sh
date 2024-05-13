c7687efd56877eec12b0622b25667f0f8ab3635a
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-13 09:18:44 +0300

    work

--- scripts/raspap_ins.sh
+++ scripts/raspap_ins.sh
@@ -9,11 +9,14 @@
 # sudo rm -rf /var/www/html
 # sudo git clone -b 2.8.7 --depth=1 https://github.com/RaspAP/raspap-webgui /var/www/html
 
-WEBROOT="/var/www/html"
-CONFSRC="$WEBROOT/config/50-raspap-router.conf"
-LTROOT=$(grep "server.document-root" /etc/lighttpd/lighttpd.conf | awk -F '=' '{print $2}' | tr -d " \"")
+# WEBROOT="/var/www/html"
+# CONFSRC="$WEBROOT/config/50-raspap-router.conf"
+# LTROOT=$(grep "server.document-root" /etc/lighttpd/lighttpd.conf | awk -F '=' '{print $2}' | tr -d " \"")
 
-HTROOT=${WEBROOT/$LTROOT}
-HTROOT=$(echo "$HTROOT" | sed -e 's/\/$//')
-awk "{gsub(\"/REPLACE_ME\",\"$HTROOT\")}1" $CONFSRC > /tmp/50-raspap-router.conf
-sudo cp /tmp/50-raspap-router.conf /etc/lighttpd/conf-available/
+# HTROOT=${WEBROOT/$LTROOT}
+# HTROOT=$(echo "$HTROOT" | sed -e 's/\/$//')
+# awk "{gsub(\"/REPLACE_ME\",\"$HTROOT\")}1" $CONFSRC > /tmp/50-raspap-router.conf
+# sudo cp /tmp/50-raspap-router.conf /etc/lighttpd/conf-available/
+
+sudo ln -s /etc/lighttpd/conf-available/50-raspap-router.conf /etc/lighttpd/conf-enabled/50-raspap-router.conf
+sudo systemctl restart lighttpd.service

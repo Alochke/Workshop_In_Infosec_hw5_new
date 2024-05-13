fc81ebeadfc43ba61be7f9e4bf8e186e60fa8e7e
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-13 08:29:05 +0300

    work

--- scripts/set_up_firewall.sh
+++ scripts/set_up_firewall.sh
@@ -1,8 +1,8 @@
 cd ~
 
 apt update && \
-    apt upgrade -y &&\
-    apt install -y wget build-essential zlib1g-dev libncurses-dev libgdbm-dev libssl-dev libsqlite3-dev libffi-dev libbz2-dev git && \
+    sudo apt upgrade -y &&\
+    sudo apt install -y wget build-essential zlib1g-dev libncurses-dev libgdbm-dev libssl-dev libsqlite3-dev libffi-dev libbz2-dev git && \
     wget https://www.python.org/ftp/python/3.7.17/Python-3.7.17.tgz  && \
     tar -xzf Python-3.7.17.tgz
 
@@ -13,6 +13,6 @@ cd /Python-3.7.17
 
 cd ..
 
-RUN rm -rf Python-3.7.17.tgz Python-3.7.17
+rm -rf Python-3.7.17.tgz Python-3.7.17
 
 

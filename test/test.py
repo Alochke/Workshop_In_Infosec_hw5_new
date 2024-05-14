c3dbf76fc84618f3fe4759f893a977876e38db4f
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-14 19:48:31 +0300

    work.

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ for i in range(1, 11):
 
 print("Max false score is: " + str(false_max))
 
-for i in range(0, 6):
+for i in range(1, 6):
     with open("true" + str(i)) as file:
         temp = guesslang.Guess().scores(file.read())['C']
         if temp < true_min:

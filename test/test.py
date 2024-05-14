3b5acc19931f6d6658abacaffe1d259f326630cc
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-14 19:46:29 +0300

    work.

--- test/test.py
+++ test/test.py
@@ -1,4 +1,20 @@
 import guesslang
 
-with open("false1") as file:
-    print(guesslang.Guess().scores(file.read()))
+false_max = 0
+true_min = 1
+
+for i in range(10):
+    with open("false" + str(i)) as file:
+        temp = guesslang.Guess().scores(file.read())['C']
+        if temp > false_max:
+            false_max = temp
+
+print("Max false score is: " + false_max)
+
+for i in range(5):
+    with open("true" + str(i)) as file:
+        temp = guesslang.Guess().scores(file.read())['C']
+        if temp < true_min:
+            true_min = temp
+
+print("Min true score is: " + true_min)

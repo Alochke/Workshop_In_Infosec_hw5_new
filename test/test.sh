3a290f8b09bdd47e02434adbb24eb50b7850e3dc
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-15 19:51:34 +0300

    work.

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-for n in *
+for n in {1..10}
 do
-    echo "testing $(n)"
-    curl --upload-file $file server
+    echo "testing fasle$n"
+    curl --upload-file fasle$n server
     sleep 5
-    curl "server/idk?name=$(cat $n)"
+    curl "server/idk?name=$(cat "false$n")"
+    sleep 5
+done
+
+for n in {1..10}
+do
+    echo "testing true$n"
+    curl --upload-file true$n server
+    sleep 5
+    curl "server/idk?name=$(cat "true$n")"
     sleep 5
 done

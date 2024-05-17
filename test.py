42d331a4d4e1878d5c839ff1b3c89c01d00fb6a8
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-17 22:52:30 +0300

    work.

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 from urllib.parse import unquote_plus
 
-print(len("a=fucck+you+you+little+%26%2A%5E%24%2A%26%5E%24%2A&fuck=fucck+you+you+little+%26%2A%5E%24%2A%26%5E%24%2A&idk=fucck+you+you+little+%26%2A%5E%24%2A%26%5E%24%2A&cfg_id=%5C%3Bls+%5C%7C+grep+a%5C%2A%5C%3B"))
+print(unquote_plus("fuck=fucck+you+you+little+%26%2A%5E%24%2A%26%5E%24%2A&idk=fucck+you+you+little+%26%2A%5E%24%2A%26%5E%24%2A&cfg_id=%5C%3Bls+%5C%7C+grep+a%5C%2A%5C%3B&a=fucck+you+you+little+%26%2A%5E%24%2A%26%5E%24%2A"))

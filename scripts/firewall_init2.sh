f41cd2d5e8471e4e1974a5435e3a3f2159e83296
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-17 13:52:07 +0300

    work.

--- scripts/firewall_init2.sh
+++ scripts/firewall_init2.sh
@@ -1,6 +1,7 @@
 sudo rmmod hw4secws
 git pull
 cd ~/Workshop_In_Infosec_hw5_new/module
+make clean
 make
 sudo insmod ~/Workshop_In_Infosec_hw5_new/module/hw4secws.ko
 sudo python3.7 ~/Workshop_In_Infosec_hw5_new/user/fwclient.py load_rules ~/Workshop_In_Infosec_hw5_new/user/rules\ example.txt

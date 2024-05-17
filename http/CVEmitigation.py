cec144bc4ded2c992524de33603bf489407e4a48
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-17 15:46:10 +0300

    work.

--- http/CVEmitigation.py
+++ http/CVEmitigation.py
@@ -75,6 +75,7 @@ while True:
                 if find != 0 and len(data[find + 4:]) == length:
                     break
             data = protect_CVE(data)
+            print(data)
 
             #if(addr[0]=='10.1.1.1'): #assume true to simplify
             insock.connect(('10.1.1.1',80))

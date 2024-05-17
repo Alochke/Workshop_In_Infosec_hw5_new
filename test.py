6e9c0ceb91a4478f42ad67492e419ad1d705c82a
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-17 23:40:48 +0300

    work

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 from urllib.parse import unquote_plus
 
-print(unquote_plus("a=%5E%24+d&logfile=%5C%3Becho+%22fuck%22+%5C%7C+sed+%22s%5C%5Cf%5C%5Cd%5C%5C%22%5C%3B&fuck=%25%24%24%25+%26%2A%5E%24%2A%26%5E%24%2A&idk=b+%25%24%23%23"))
+# print(len("fuck=&idk=&a=&cfg_id=%5C%3Btouch+idk+%5C%26%5C%26+cat+idk%5C%3B"))
+
+a = b'a'
+b = a
+a = b'b'
+
+print(a == b)

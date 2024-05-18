1f62490f4cb1bc13d70228cd0577ca5a7593b4dc
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-18 20:42:52 +0300

    work.

--- smtp/smtpproxy.py
+++ smtp/smtpproxy.py
@@ -1,54 +1,74 @@
+#!/usr/bin/python3
 
-
+from _typeshed import ReadableBuffer
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from os import open, write, close, O_WRONLY
 from struct import pack, calcsize
 from ipaddress import ip_address
-import guesslang
+from threading import Thread, Lock
 
 MITM_STRUCT = '!LHLHH'
 MITM_SIZE = calcsize(MITM_STRUCT)
 
-while True:
-    data = bytearray()
-    outsock = socket(AF_INET, SOCK_STREAM)
-    with socket(AF_INET, SOCK_STREAM) as insock:
-        insock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # TODO remove?
-        insock.bind(('10.1.1.3', 205))
-        print('http socket bound')
-        insock.listen()
+class SockPlus(socket):
+    lock = Lock()
+    is_closed = False
+
+
+    def factor(self, func: function):
+        self.lock.acquire()
+        if not self.is_closed:
+            return func()
+        self.lock.release()
+
+    def close(self) -> None:
+        self.lock.acquire()
+        if not self.is_closed:
+            self.is_closed = True
+            self.socket.close()
+        self.lock.release()
+
+
+def handle(sock1: SockPlus, sock2: SockPlus):
+    print('Started request thread')
+    while True:
+        data = bytearray()
+        while True:
+            inp = sock1.factor(lambda x: sock1.socket.recv(4096))
+            if not inp:
+                sock1.close()
+                return
+            data += inp
+            if data.endswith(b'\r\n'): break # FTP command termination
+
+        sock2.factor(lambda x: sock2.socket.sendall())
+
+
+with socket(AF_INET, SOCK_STREAM) as insock:
+    insock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # TODO remove?
+    insock.bind(('10.1.1.3', 205))
+    print('ftp socket bound')
+    
+    insock.listen()
+    while True:        
+        print('Waiting for connection')
         conn, addr = insock.accept()
-        print('connection established from '+str(addr))
-        with conn:
-            while True:
-                print('receiving request')
-                inp = conn.recv(4096)            
-                if not inp: break
-                data += inp
-                if data.endswith(b'\r\n'):
-                    break
+        print('connection established from '+str(addr))     
+                  
+        outsock=socket(AF_INET, SOCK_STREAM)
+        outsock.connect(('10.1.2.2',25))
+        # Update mitm port in kernel
+        (_,mitmport)=outsock.getsockname()
+        mitmdriver=open('/sys/class/fw/conns/mitm', O_WRONLY)
+        write(mitmdriver,pack(MITM_STRUCT,int(ip_address(addr[0])),addr[1],int(ip_address('10.1.2.2')),25,mitmport))
+        close(mitmdriver)    
 
-            # print(data) # for debug
-
-            #if(addr[0]=='10.1.1.1'): #assume true to simplify
-            outsock.connect(('10.1.2.2',25))
-
-
-            # Update mitm port in kernel
-            (_,mitmport)=outsock.getsockname()
-            mitmdriver=open('/sys/class/fw/conns/mitm', O_WRONLY)
-            write(mitmdriver,pack(MITM_STRUCT,int(ip_address(addr[0])),addr[1],int(ip_address('10.1.2.2')),25,mitmport))
-            close(mitmdriver)
-
-            outsock.sendall(data)    
-
-# ---------------- HANDLE RESPONSE -----------------------
-            data = bytearray()
-            while True:
-                print('receiving response header')
-                inp = outsock.recv(4096)            
-                if not inp: break
-                data += inp
-            
-
-    outsock.close()
+        sock1 = SockPlus()
+        sock2 = SockPlus()
+        sock1.socket = conn
+        sock2.socket = outsock
+        rqt=Thread(target=handle,args=(sock1, sock2))
+        rpt=Thread(target=handle,args=(sock1, sock2))
+        rqt.start()
+        rpt.start()
+                
--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-from urllib.parse import unquote_plus
-
-# print(len("fuck=&idk=&a=&cfg_id=%5C%3Btouch+idk+%5C%26%5C%26+cat+idk%5C%3B"))
-
-a = b'a'
-b = a
-a = b'b'
-
-print(a == b)
+lambda x: print('hello')

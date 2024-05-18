ac08132e48d9852263841226102cc7e4872c02db
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-18 16:19:25 +0300

    work.

--- scripts/firewall_init2.sh
+++ scripts/firewall_init2.sh
@@ -4,4 +4,4 @@ cd ~/Workshop_In_Infosec_hw5_new/module
 make
 sudo insmod ~/Workshop_In_Infosec_hw5_new/module/hw4secws.ko
 sudo python3.7 ~/Workshop_In_Infosec_hw5_new/user/fwclient.py load_rules ~/Workshop_In_Infosec_hw5_new/user/rules\ example.txt
-sudo python3.7 ~/Workshop_In_Infosec_hw5_new/ftp/ftpproxy.py
+sudo python3.7 ~/Workshop_In_Infosec_hw5_new/smtp/smtpproxy.py

95b060acfad28a7bedd4f73c8f50e67b97d340c2
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-19 15:29:01 +0300

    work.

--- smtp/smtpproxy.py
+++ smtp/smtpproxy.py
@@ -71,7 +71,7 @@ while True:
                                 break
                     print(data2)
                     print("Guess is: " + str(guesslang.Guess().scores(data2.decode(errors='replace'))['C']))
-                    if has_C_code(data2) and inp:
+                    if has_C_code(data2) and data2:
                         print(b"C code detected.")
                         try:
                             conn.sendall(b'C code has been sent, terminating connection.\r\n')

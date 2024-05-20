2dea1e016c7ff171f7b01d71bd45f73f394dc2a0
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-20 16:56:33 +0300

    work.

--- test/test.py
+++ test/test.py
@@ -1,22 +1,5 @@
-from time import sleep
+import guesslang
 
-false_max = 0
-true_min = 1
-
-for i in range(1, 11):
-    with open("false" + str(i)) as file:
-        print("testing false"  + str(i))
-        exec("curl server/idk?data=" + file.read())
-        sleep(5)
-        file.seek(0)
-        exec("curl --upload-file false" + str(i)+  " server")
-        sleep(5)
-
-for i in range(1, 11):
-    with open("true" + str(i)) as file:
-        print("testing true"  + str(i))
-        exec("curl server/idk?data=" + file.read())
-        sleep(5)
-        file.seek(0)
-        exec("curl --upload-file true" + str(i)+  " server")
-        sleep(5)
+for i in range(1, 5):
+    with open("falsefalse" + str(i)) as file:
+        print(guesslang.Guess().scores(file.read())['C'])

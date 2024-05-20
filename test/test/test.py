3637c510b0a653ac25e754268774b45cfa1909de
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-05-20 18:46:43 +0300

    work.

--- test/test/test.py
+++
@@ -1,5 +0,0 @@
-import guesslang
-
-for i in range(1, 5):
-    with open("falsefalse" + str(i)) as file:
-        print(guesslang.Guess().scores(file.read())['C'])
--- test/test/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "helo idk
-mail from: idk
-rcpt to: root
-data
-$(cat $1)
-." |telnet 10.1.2.2 25
